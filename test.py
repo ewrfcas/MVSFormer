7af3966484d3e1ff88e1282f1eb38bae6bbc9d66
maybeLx <108978824+maybeLx@users.noreply.github.com>
2022-12-16 21:39:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -428,9 +428,7 @@ def filter_depth(pair_folder, scan_folder, out_folder, plyfilename):
         idx_img = fusion.get_pixel_grids(*ref_depth_ave.size()[-2:]).unsqueeze(0)
         idx_cam = fusion.idx_img2cam(idx_img, ref_depth_ave, sample['ref_cam'])
         points = fusion.idx_cam2world(idx_cam, sample['ref_cam'])[..., :3, 0].permute(0, 3, 1, 2)
-        # cam_center = (- sample['ref_cam'][:,0,:3,:3].transpose(-2,-1) @ sample['ref_cam'][:,0,:3,3:])[...,0]
-        # dir_vecs = cam_center.unsqueeze(-1).unsqueeze(-1) - points
-
+       
         points_np = points.cpu().data.numpy()
         mask_np = mask.cpu().data.numpy().astype(np.bool)
         # dir_vecs = dir_vecs.cpu().data.numpy()
@@ -465,12 +463,96 @@ def filter_depth(pair_folder, scan_folder, out_folder, plyfilename):
     print("saving the final model to", plyfilename)
 
 
+
+def dynamic_filter_depth(pair_folder, scan_folder, out_folder, plyfilename):
+    tt_dataset = TTDataset(pair_folder, scan_folder, n_src_views=10)
+    sampler = SequentialSampler(tt_dataset)
+    tt_dataloader = DataLoader(tt_dataset, batch_size=1, shuffle=False, sampler=sampler, num_workers=2,
+                               pin_memory=True, drop_last=False)
+    views = {}
+    prob_threshold = args.conf
+    prob_threshold = [float(p) for p in prob_threshold.split(',')]
+    for batch_idx, sample_np in enumerate(tt_dataloader):
+        num_src_views = sample_np['src_depths'].shape[1]
+        dy_range = num_src_views + 1 # 10
+        sample = tocuda(sample_np)
+
+        if args.combine_conf:
+            prob_mask = sample['ref_conf'] > prob_threshold[0]
+        else:
+            prob_mask = fusion.prob_filter(sample['ref_conf'], prob_threshold)
+
+
+        ref_depth = sample['ref_depth'] # [n 1 h w ]
+        device = ref_depth.device
+        reproj_xyd = fusion.get_reproj_dynamic(
+            *[sample[attr] for attr in ['ref_depth', 'src_depths', 'ref_cam', 'src_cams']])
+        # reproj_xyd   nv 3 h w
+
+        vis_masks, vis_mask = fusion.vis_filter_dynamic(sample['ref_depth'], reproj_xyd, dist_base=args.dist_base,  # 4 1300
+                                                        rel_diff_base=args.rel_diff_base)
+
+        # mask reproj_depth
+        reproj_depth = reproj_xyd[:,:,-1] # [1 v h w]
+        reproj_depth[~vis_mask.squeeze(2)] = 0  # [n v h w ]
+        geo_mask_sums = vis_masks.sum(dim=1) # 0~v
+        geo_mask_sum = vis_mask.sum(dim=1)
+        depth_est_averaged = (torch.sum(reproj_depth,dim=1,keepdim=True) + ref_depth) / (geo_mask_sum + 1)# [1,1,h,w]
+        geo_mask = geo_mask_sum >= dy_range  # all zero
+        for i in range(2, dy_range):
+            geo_mask = torch.logical_or(geo_mask, geo_mask_sums[:,i - 2]>=i)
+
+        mask = fusion.bin_op_reduce([prob_mask, geo_mask], torch.min)
+        idx_img = fusion.get_pixel_grids(*depth_est_averaged.size()[-2:]).unsqueeze(0)
+        idx_cam = fusion.idx_img2cam(idx_img, depth_est_averaged, sample['ref_cam'])
+        points = fusion.idx_cam2world(idx_cam, sample['ref_cam'])[..., :3, 0].permute(0, 3, 1, 2)
+
+        points_np = points.cpu().data.numpy()
+        mask_np = mask.cpu().data.numpy().astype(np.bool)
+
+        ref_img = sample_np['ref_img'].data.numpy()
+        for i in range(points_np.shape[0]):
+            print(np.sum(np.isnan(points_np[i])))
+            p_f_list = [points_np[i, k][mask_np[i, 0]] for k in range(3)]
+            p_f = np.stack(p_f_list, -1)
+            c_f_list = [ref_img[i, k][mask_np[i, 0]] for k in range(3)]
+            c_f = np.stack(c_f_list, -1) * 255
+
+            ref_id = str(sample_np['ref_id'][i].item())
+            views[ref_id] = (p_f, c_f.astype(np.uint8))
+            print("processing {}, ref-view{:0>2}, photo/geo/final-mask:{}/{}/{}".format(scan_folder, int(ref_id),
+                                                                                        prob_mask[
+                                                                                            i].float().mean().item(),
+                                                                                        geo_mask[
+                                                                                            i].float().mean().item(),
+                                                                                        mask[i].float().mean().item()))
+
+    print('Write combined PCD')
+    p_all, c_all = [np.concatenate([v[k] for key, v in views.items()], axis=0) for k in range(2)]
+
+    vertexs = np.array([tuple(v) for v in p_all], dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
+    vertex_colors = np.array([tuple(v) for v in c_all], dtype=[('red', 'u1'), ('green', 'u1'), ('blue', 'u1')])
+
+    vertex_all = np.empty(len(vertexs), vertexs.dtype.descr + vertex_colors.dtype.descr)
+    for prop in vertexs.dtype.names:
+        vertex_all[prop] = vertexs[prop]
+    for prop in vertex_colors.dtype.names:
+        vertex_all[prop] = vertex_colors[prop]
+
+    el = PlyElement.describe(vertex_all, 'vertex')
+    PlyData([el]).write(plyfilename)
+    print("saving the final model to", plyfilename)
+
+
 def pcd_filter_worker(scan):
     save_name = '{}.ply'.format(scan)
     pair_folder = os.path.join(args.testpath, scan)
     scan_folder = os.path.join(args.outdir, scan)
     out_folder = os.path.join(args.outdir, scan)
-    filter_depth(pair_folder, scan_folder, out_folder, os.path.join(args.outdir, save_name))
+    if args.filter_method == 'pcd':
+        filter_depth(pair_folder, scan_folder, out_folder, os.path.join(args.outdir, save_name))
+    else:
+        dynamic_filter_depth(pair_folder, scan_folder, out_folder, os.path.join(args.outdir, save_name))
 
 
 def pcd_filter(testlist):
@@ -498,11 +580,10 @@ if __name__ == '__main__':
     save_depth(testlist, config)
 
     # step2. filter saved depth maps with photometric confidence maps and geometric constraints
-    if args.filter_method == "pcd":
-        # support multi-processing, the default number of worker is 4
-        pcd_filter(testlist)
-    elif args.filter_method == 'dpcd':
-        raise NotImplementedError
+    if args.filter_method == "pcd" or args.filter_method == "dpcd":
+    # support multi-processing, the default number of worker is 4
+       pcd_filter(testlist)
+    
     elif args.filter_method == 'gipuma':
         prob_threshold = args.prob_threshold
         prob_threshold = [float(p) for p in prob_threshold.split(',')]
